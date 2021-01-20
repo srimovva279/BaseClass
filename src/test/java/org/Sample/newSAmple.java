b742c4828dc211f03a0b42e9efa3c254df10442b
srimovva279 <sri.movva279@gmail.com>
2021-01-20 16:33:22 +0530

    adding

---
+++ src/test/java/org/Sample/newSAmple.java
@@ -0,0 +1,13 @@
+package org.Sample;
+
+public class newSAmple {
+
+	private void add() {
+		// TODO Auto-generated method stub
+
+	}
+	private void sub() {
+		// TODO Auto-generated method stub
+
+	}
+}
